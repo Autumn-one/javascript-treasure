72ea89db6eff63efb31fc2b23d7d2232e3d82bc7
Autumn <718879459@qq.com>
2024-04-03 17:59:06 +0800

    提交

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# README.md
+# javascript-treasure
+
+**黑人是病毒！不能让世界感染病毒！反黑的声音不该被禁止！世界不应该再出现第二个法国！**
+
+（所有人应该明白，当一群垃圾开源项目都在官网挂上“黑命贵”宣言的时候，没有项目挂上反黑宣言，这很怪异！援默行动是否过于浮夸了？既然那一部分搞垃圾技术的low b援默，那么反默宣言从我开始吧）
+
+一个javascript 函数工具集合
+
+* throttle 节流，支持动态节流
+* debounce 防抖，支持动态防抖
+* idle 闲时执行
+* async_idle 闲时执行async版本
+* retry 重试函数
+* promise_timeout 给任意的promise添加最小时间和最大时间
+* promise_queue 多个promise排队执行
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,119 +1,123 @@
-import { expect, test, vi } from "vitest"
+import { expect, test, vi, describe } from "vitest"
 import { promise_timeout } from "../src/promise_tools";
 
 
-test( "promise_timeout-最小等待时间300", async function ()
+describe.concurrent( "promise_timeout函数测试", () =>
 {
-    const start = +new Date();
-    const ret = await promise_timeout(
-        new Promise( ( resolve, reject ) =>
-        {
-            setTimeout( () => resolve( 66 ), 100 )
-        } ),
-        300
-    )
-    const end = +new Date();
-    const cha = end - start;
-    expect( true ).toBe( cha >= 300 && cha < 400 )
-    expect( ret ).toBe( 66 )
-} );
-
-test( "promise_timeout-最大等待时间300", async function ()
-{
-    const start = +new Date();
-    let ret;
-    try
+    test( "promise_timeout-最小等待时间300", async function ()
     {
-        ret = await promise_timeout(
+        const start = +new Date();
+        const ret = await promise_timeout(
             new Promise( ( resolve, reject ) =>
             {
-                setTimeout( () => resolve( 66 ), 1200 )
+                setTimeout( () => resolve( 66 ), 100 )
             } ),
-            null,
             300
         )
-    }
-    catch ( e )
+        const end = +new Date();
+        const cha = end - start;
+        expect( true ).toBe( cha >= 300 && cha < 400 )
+        expect( ret ).toBe( 66 )
+    } );
+
+    test( "promise_timeout-最大等待时间300", async function ()
     {
-        ret = e;
-    }
-
-    const end = +new Date();
-    const cha = end - start;
-    expect( true ).toBe( cha > 300 && cha < 400 )
-    expect( ret.type ).toBe( "timeout" )
-} )
-
-
-test( "promise_timeout-最小等待200最大超时300小于200", async function ()
-{
-    const start = +new Date();
-    let ret = await promise_timeout(
-        new Promise( ( resolve, reject ) =>
+        const start = +new Date();
+        let ret;
+        try
         {
-            setTimeout( () => resolve( 66 ), 100 )
-        } ),
-        200,
-        300
-    )
-    const end = +new Date();
-    const cha = end - start;
-    expect( true ).toBe( cha >= 200 && cha < 300 )
-    expect( ret ).toBe( 66 )
-} )
+            ret = await promise_timeout(
+                new Promise( ( resolve, reject ) =>
+                {
+                    setTimeout( () => resolve( 66 ), 1200 )
+                } ),
+                null,
+                300
+            )
+        }
+        catch ( e )
+        {
+            ret = e;
+        }
 
-test( "promise_timeout-最小等待200最大超时300大于300", async function ()
-{
-    const start = +new Date();
-    let ret;
-    try
+        const end = +new Date();
+        const cha = end - start;
+        expect( true ).toBe( cha > 300 && cha < 400 )
+        expect( ret.type ).toBe( "timeout" )
+    } )
+
+
+    test( "promise_timeout-最小等待200最大超时300小于200", async function ()
     {
-        ret = await promise_timeout(
+        const start = +new Date();
+        let ret = await promise_timeout(
             new Promise( ( resolve, reject ) =>
             {
-                setTimeout( () => resolve( 66 ), 360 )
+                setTimeout( () => resolve( 66 ), 100 )
             } ),
             200,
             300
         )
-    }
-    catch ( e )
+        const end = +new Date();
+        const cha = end - start;
+        expect( true ).toBe( cha >= 200 && cha < 300 )
+        expect( ret ).toBe( 66 )
+    } )
+
+    test( "promise_timeout-最小等待200最大超时300大于300", async function ()
     {
-        ret = e;
-    }
+        const start = +new Date();
+        let ret;
+        try
+        {
+            ret = await promise_timeout(
+                new Promise( ( resolve, reject ) =>
+                {
+                    setTimeout( () => resolve( 66 ), 360 )
+                } ),
+                200,
+                300
+            )
+        }
+        catch ( e )
+        {
+            ret = e;
+        }
 
-    const end = +new Date();
-    const cha = end - start;
-    expect( true ).toBe( cha >= 300 && cha < 400 )
-    expect( ret.type ).toBe( "timeout" )
-} )
+        const end = +new Date();
+        const cha = end - start;
+        expect( true ).toBe( cha >= 300 && cha < 400 )
+        expect( ret.type ).toBe( "timeout" )
+    } )
 
 
-test( "promise_timeout-错误优先", async function ()
-{
-    const start = +new Date();
-    let ret;
-    try
+    test( "promise_timeout-错误优先", async function ()
     {
-        ret = await promise_timeout(
-            new Promise( ( resolve, reject ) =>
-            {
-                setTimeout( () => reject( 66 ), 100 )
-            } ),
-            200,
-            300,
-            true
-        )
-    }
-    catch ( e )
-    {
-        ret = e;
-    }
+        const start = +new Date();
+        let ret;
+        try
+        {
+            ret = await promise_timeout(
+                new Promise( ( resolve, reject ) =>
+                {
+                    setTimeout( () => reject( 66 ), 100 )
+                } ),
+                200,
+                300,
+                true
+            )
+        }
+        catch ( e )
+        {
+            ret = e;
+        }
 
-    const end = +new Date();
-    const cha = end - start;
-    expect( true ).toBe( cha >= 100 && cha < 200 )
-    expect( ret ).toBe( 66 )
+        const end = +new Date();
+        const cha = end - start;
+        expect( true ).toBe( cha >= 100 && cha < 200 )
+        expect( ret ).toBe( 66 )
+    } )
+
 } )
 
 
