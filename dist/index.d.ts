91ffe1bd5ed93a60d7c469cede8bd7bc71b47906
Autumn <718879459@qq.com>
2024-04-03 14:37:56 +0800

    修复问题，提交retry函数

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,23 @@
 /**
+ * 管理promise队列，让promise按照队列顺序来请求, 每个promise都会有一个默认的timeout
+ * @fnName promise_queue
+ * @param pss
+ * @param timeout
+ */
+declare function promise_queue(pss: Promise<any>[], timeout?: number): Promise<any>;
+/**
+ * 给promise添加最低时长与最大超时时间
+ * @fnName promise_timeout
+ * @param ps 要操作的promise
+ * @param min_time 最小时间如果不设置就设置为null
+ * @param max_time 最大的超时时间如果不设置就设置为null
+ * @param error_first 是否错误优先，如果错误优先那么出现错误会忽略最小时间
+ */
+declare function promise_timeout(ps: Promise<any>, min_time?: number | null, max_time?: number | null, error_first?: boolean): Promise<any>;
+
+declare function idle(callback: Function, delay?: number): number;
+declare function async_idle(delay?: number): Promise<unknown>;
+/**
  * 防抖函数
  * @param func 要被防抖的函数
  * @param wait 防抖等待时间
@@ -14,5 +33,9 @@ declare function debounce(func: Function, wait: number, type?: "t-d" | "i-d" | "
 declare function throttle(func: Function, wait: number | {
     time: number;
 }): Function;
+declare function retry(fn: (...args: any[]) => Promise<any>, options?: {
+    times: number;
+    on_failed: (err: any, count: any) => any;
+}): Promise<any>;
 
-export { debounce, throttle };
+export { async_idle, debounce, idle, promise_queue, promise_timeout, retry, throttle };
